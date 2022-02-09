25d6bd80124be13c4bc84b7dff93dbaf078236ee
Nikki Sato <nikki.satopdx@gmail.comgit config --global user.email nikki.satopdx@gmail.com>
2022-02-08 20:53:16 -0800

    finished section 5

--- src/components/index.js
+++ src/components/index.js
@@ -6,3 +6,6 @@ export * from './Input';
 export * from './Button';
 export * from './Cart';
 export * from './Header';
+export * from './CartContents';
+export * from './QuantityAdjuster';
+export * from './RemoveLineItem';
